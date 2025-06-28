16c9c26d67201942ba4f4d00a1fb8e76afd0af08
Composure <xutong1107@gmail.com>
2025-06-28 23:09:23 +0800

    feat(config): mapper 注解

--- src/main/java/com/example/dal/mapper/SysConfigMapper.java
+++ src/main/java/com/example/dal/mapper/SysConfigMapper.java
@@ -2,9 +2,11 @@ package com.example.dal.mapper;
 
 import com.example.dal.entity.SysConfigEntity;
 import com.example.web.controller.SysConfigPageReq;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface SysConfigMapper {
     List<SysConfigEntity> queryConfigByPage(SysConfigPageReq req);
 }
